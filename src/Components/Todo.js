fc7f1668b12982c850fb8526a85db974a9f9c8ae
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2024-01-13 19:55:05 +0530

    Added Delete functionality and added local storage

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,6 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Todo() {
+
+    const getlocalStorage = () => {
+        const lists = localStorage.getItem('Todo_list')
+
+        if (lists) {
+            return JSON.parse(lists)
+        }
+        else {
+            return [];
+        }
+    }
+
+
+    const [inputData, setInputData] = useState("")
+    // const [items, setItems] = useState([])
+    const [items, setItems] = useState(getlocalStorage())
+    const [storage, setStorage] = useState()
+
+
+    // Adding New Item....
+    const addItems = () => {
+        if (!inputData) {
+            alert("Add items")
+        }
+        else {
+
+            const myNewInputData = {
+                id: new Date().getTime().toString(),
+                name: inputData
+            }
+
+            console.log("User input data  = " + inputData + "\n");
+            console.log("User input data  = " + myNewInputData.name + "\nId : " + myNewInputData.id);
+
+            setItems([...items, myNewInputData])
+            setInputData("")
+        }
+    }
+
+    // Removing item...
+    const removeItems = (index) => {
+        const updatedItems = items.filter((currentElem) => {
+            return currentElem.id !== index;
+        })
+        setItems(updatedItems)
+    }
+
+    // Removing all the items
+    const removeAll = () => {
+        setItems([])
+    }
+
+    useEffect(() => {
+        localStorage.setItem("Todo_list", JSON.stringify(items))
+    }, [items])
+
+
+
     return (
         <div>
 
@@ -19,58 +77,45 @@ export default function Todo() {
 
                         <div className="input-text">
 
-                            <input type="text" name="" id="" placeholder='✍ Add Your note here' />
-                            <i className="fas fa-plus"></i>
+                            <input type="text" name="" id="" placeholder='✍ Add Your note here' value={inputData} onChange={(event) => { setInputData(event.target.value) }} />
+                            <i className="fas fa-plus" onClick={addItems}></i>
                         </div>
 
                         <div className="items-list">
 
+                            {
+                                items.map((currentElem) => {
+                                    return (
+                                        < div className="items" key={currentElem.id} >
+                                            {/* currentElem me value kaise ghus gye */}
 
-                            <div className="items">
+                                            <div className="content-item">
+                                                <h1>{currentElem.name}</h1>
+                                            </div>
 
-                                <div className="content-item">
-                                    <h1>Amazon</h1>
-                                </div>
+                                            <div className="icons">
 
-                                <div className="icons">
+                                                <div className="edit-icon">
+                                                    <i className="far fa-edit"></i>
+                                                </div>
 
-                                    <div className="edit-icon">
-                                        <i className="far fa-edit"></i>
-                                    </div>
+                                                <div className="delete-icon">
+                                                    <i className="far fa-trash-alt" onClick={() => { removeItems(currentElem.id) }}></i>
+                                                </div>
 
-                                    <div className="delete-icon">
-                                        <i className="far fa-trash-alt"></i>
-                                    </div>
+                                            </div>
 
-                                </div>
+                                        </div>
+                                    )
+                                })
+                            }
 
-                            </div>
-
-                            <div className="items">
-
-                                <div className="content-item">
-                                    <h1>Google</h1>
-                                </div>
-
-                                <div className="icons">
-
-                                    <div className="edit-icon">
-                                        <i className="far fa-edit"></i>
-                                    </div>
-
-                                    <div className="delete-icon">
-                                        <i className="far fa-trash-alt"></i>
-                                    </div>
-
-                                </div>
-
-                            </div>
 
                         </div>
 
                         <div className="remove-all-btn">
 
-                            <button>Remove all</button>
+                            <button onClick={removeAll}>Remove all</button>
 
                         </div>
                     </div>
@@ -80,6 +125,6 @@ export default function Todo() {
 
             </div>
 
-        </div>
+        </div >
     )
 }
